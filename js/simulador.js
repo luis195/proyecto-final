a9d0cfcb3e63d0ef69ef932d519d28416a565bb4
Luis Andrade <luisandrade195@gmail.com>
2021-09-13 18:00:23 -0300

    se acomoda el boton de agregar servicio, resta el calculo total.

--- js/simulador.js
+++ js/simulador.js
@@ -1,29 +1,3 @@
-
-class Servicio {
-    constructor(tipo, cantidad, precio) {
-        this.tipo = tipo;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-}
-
-const servicios = [];
-
-function ingresarServicio(){
-        switch (valorServicio){
-            case 1:
-                servicios.push(new Servicio("Ilustracion", "1", "20000"));
-                break;
-            case 2:
-                servicios.push(new Servicio("Asesoria", "1", "10000"));
-                break;
-            case 3:
-                servicios.push(new Servicio("Creacion de Contenido", "1", "8000"));
-                break;
-            case 4:
-                servicios.push(new Servicio("Branding", "1", "13000"))
-        }
-}
 /*
 let cuentaFinal = ingresarServicio()
 let total = JSON.stringify(servicios);
@@ -47,38 +21,70 @@ alert("Sus serficios seleccionados son: \n " + total + " \n y el total sera: \n"
 
 
 }*/
-//$("#listaServicios").onchange(
+class Servicio {
+    constructor(tipo, cantidad, precio) {
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+}
+
+const servicios = [];
+let calculo;
 let listaServicios = document.getElementById("listaServicios");
 let agregar = document.getElementById("agregar")
+let cuenta = document.getElementById("calcular")
 
-    function escogerServcio() {
-        let valorServicio = listaServicios.value;
-        // encontrar el textbox
-        let textBox = document.getElementById("campoServicio");
-        //console.log(valorServicio)
-        switch (valorServicio) {
+function ingresarServicio(){
+        switch (listaServicios.value){
             case "1":
-                textBox.value = "Ilustracion";
+                servicios.push(new Servicio("Ilustracion", "1", "20000"));
                 break;
-
             case "2":
-                textBox.value = "Asesoria";
+                servicios.push(new Servicio("Asesoria", "1", "10000"));
                 break;
-
             case "3":
-                textBox.value = "Creacion de Contenido";
+                servicios.push(new Servicio("Creacion de Contenido", "1", "8000"));
                 break;
-
             case "4":
-                textBox.value = "Branding";
-
+                servicios.push(new Servicio("Branding", "1", "13000"))
         }
+        return servicios
+}
 
-        //agregar.addEventListener("click", ingresarServicio)
-        console.log(servicios)
+// se encarga de manejar el evento de las listas
+function escogerServicio() {
+    let valorServicio = listaServicios.value;
+    // encontrar el textbox
+    let textBox = document.getElementById("campoServicio");
+    //console.log(valorServicio)
+    switch (valorServicio) {
+        case "1":
+            textBox.value = "Ilustracion";
+            break;
+
+        case "2":
+            textBox.value = "Asesoria";
+            break;
+
+        case "3":
+            textBox.value = "Creacion de Contenido";
+            break;
+
+        case "4":
+            textBox.value = "Branding";
+
     }
-listaServicios.onchange = escogerServcio;
-escogerServcio();
-//)
+
+    agregar.onclick = ingresarServicio
+    console.log(servicios)
+}
+
+function calculoTarifa(){
+    for (let i in servicios){
+        cuenta = servicios[i]["cuenta"]
+    }
+}
+listaServicios.onchange = escogerServicio;
 
 
